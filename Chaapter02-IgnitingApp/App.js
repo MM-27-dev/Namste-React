56c4a4e709b473df61ef9bc90781db70bc5c4cbc
MM-27-dev <monikamuniraju27@gmail.com>
2025-03-15 21:14:36 +0530

    Added Part1 of Igniting our App Episode

---
+++ Chaapter02-IgnitingApp/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
+
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+root1.render(parent);

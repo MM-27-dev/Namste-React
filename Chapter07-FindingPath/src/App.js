e91dc2183d208c6e752640af35c7ba18a2b43ff5
MM-27-dev <monikamuniraju27@gmail.com>
2025-04-08 10:44:43 +0530

    updated the project

--- Chapter07-FindingPath/src/App.js
+++ Chapter07-FindingPath/src/App.js
@@ -2,19 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+// import { createBrowserRouter } from "react-router-dom";
 
 // React.createElement => ReactElemnt-Js Object => HTML Element (when we render it)
-
 
 const AppLayout = () => {
   return (
     <div className="app">
-      {/* Header */}
       <Header />
       <Body />
-      {/* Body 
-      Footer */}
     </div>
   );
 };
